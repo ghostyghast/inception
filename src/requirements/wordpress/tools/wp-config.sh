f142d8f2dcc8cab8335e60c5ab5d6db1e62792ae
ghostyghast <antoinethebestgamer@gmail.com>
2025-08-13 15:14:09 +0800

    fix: running wp-config runs as entrypoint to ensure correct env replacement and running php command in script

--- src/requirements/wordpress/tools/wp-config.sh
+++ src/requirements/wordpress/tools/wp-config.sh
@@ -6,3 +6,5 @@ sed -i "s/database_name_here/$MARIADB_DATABASE/" /var/www/html/wp-config.php
 sed -i "s/password_here/$MARIADB_PASSWORD/" /var/www/html/wp-config.php
 sed -i "s/username_here/$MARIADB_USER/" /var/www/html/wp-config.php
 sed -i "s/localhost/$MARIADB_HOSTNAME/" /var/www/html/wp-config.php
+
+php-fpm83 -F
