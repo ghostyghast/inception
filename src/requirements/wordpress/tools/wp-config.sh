ece12d133910b20aa2db44288699b8c7ff04d64a
ghostyghast <antoinethebestgamer@gmail.com>
2025-08-19 17:51:20 +0800

    feat: creating wordpress user in wp config script

--- src/requirements/wordpress/tools/wp-config.sh
+++ src/requirements/wordpress/tools/wp-config.sh
@@ -7,6 +7,8 @@ if ! wp core is-installed --path=/var/www/html; then
     wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=$MARIADB_HOSTNAME --locale=en_GB
     echo "installing wp..."
     wp core install --title=inception --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --locale=en_GB --admin_email=amaligno@student.42kl.edu.my
+    echo "creating user"
+    wp user create $WP_USER "$WP_USER"@mail.com --user_pass=$WP_USER_PASSWORD 
 else
     echo "wp already installed!"
 fi
