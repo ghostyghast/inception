bbbfd467dd2fef112e9d6defd39e92e2f9fc8854
ghostyghast <antoinethebestgamer@gmail.com>
2025-08-27 16:15:50 +0800

    fix: linuxgsm creating config file properly now

---
+++ src/bonus/linuxgsm/tools/setup.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+cd
+if [ ! -f ./$SERVER ]; then
+    echo "$SERVER not installed"
+    curl -Lo linuxgsm.sh https://linuxgsm.sh
+    chmod +x linuxgsm.sh
+    bash linuxgsm.sh $SERVER
+else
+    echo "$SERVER already installed"
+fi
+
+echo creating config
+export CONF_PATH="$HOME/lgsm/config-lgsm/$SERVER"
+echo conf path: $CONF_PATH
+cat $CONF_PATH/_default.cfg > $CONF_PATH/$SERVER.cfg
+sed -i "s/serverpassword=\"\"/serverpassword=\"$PASSWORD\"/" $CONF_PATH/$SERVER.cfg
+
+echo "installing dependencies..."
+./$SERVER auto-install > /dev/null
+
+echo "Starting $SERVER"
+./$SERVER start
+./${GAMESERVER} details
+tail -n 3 -q -f ~/log/script/*
